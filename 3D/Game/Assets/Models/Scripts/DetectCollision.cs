c383089140ce64dc8a446615abb60ce781420c8b
IsabelDiazDominguez <isabel.diaz.dominguez@udc.es>
2018-04-12 21:49:02 +0200

    Colision pocion
    Desaparece al tocarse

---
+++ 3D/Game/Assets/Models/Scripts/DetectCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision: MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "potion")
+        {
+            Destroy(col.gameObject);
+
+            //Dar vida
+        }
+    }
+}
