a2afd27428ad5e4f91fc8d2fc70b5d70c56324be
Isabel <isabeldido@gmail.com>
2018-04-25 19:58:19 +0200

    Barra de vida
    Canvas simple de la barra de vida.

---
+++ 3D/Game/Assets/Models/Scripts/PlayerHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+    public float TotalHp;
+    public float actualHp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+}
