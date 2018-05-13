5ace9d705ed9781090e1d5d46c5d1ff85b1ef159
drivera1 <danir964@gmail.com>
2018-05-13 02:21:46 +0200

    Sangrado de enemigos
    Los enemigos sangran dónde se les dispara

--- 3D/Game/Assets/Models/Scripts/Combat/DetectHit.cs
+++ 3D/Game/Assets/Models/Scripts/Combat/DetectHit.cs
@@ -5,39 +5,17 @@ using UnityEngine;
 public class DetectHit : MonoBehaviour {
 	CharacterStats enemyStats;
 	CharacterStats playerStats;
-	float timer=0.0f;
-	public float invulnerability=1.0f;
-
 
 	void OnTriggerEnter(Collider col){
 
 		if (col.gameObject.tag == "hitMarker")
 		{
-			print ("COLL");
 			if (((EnemyAttackTimer)col.gameObject.GetComponent(typeof(EnemyAttackTimer))).canAttack()){
-				print ("ATTACK");
 				playerStats = this.gameObject.GetComponentInChildren<CharacterStats> ();
 				enemyStats = col.gameObject.GetComponentInParent<CharacterStats> ();
 				Stat enemyDamage = enemyStats.damage;
 				playerStats.TakeDamage (enemyDamage.getValue ());
 			}
-			/*if (timer >= invulnerability) {
-				playerStats = this.gameObject.GetComponentInChildren<CharacterStats> ();
-				enemyStats = col.gameObject.GetComponentInParent<CharacterStats> ();
-				Stat enemyDamage = enemyStats.damage;
-				playerStats.TakeDamage (enemyDamage.getValue ());
-				timer = 0.0f;
-			}*/
 		}
-	}
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {		
-		timer += Time.deltaTime;
 	}
 }
--- 3D/Game/Assets/Models/Scripts/Stats/EnemyStats.cs
+++ 3D/Game/Assets/Models/Scripts/Stats/EnemyStats.cs
@@ -4,6 +4,7 @@ public class EnemyStats : CharacterStats {
 
 	GameObject player;
 	CharacterStats playerStats;
+	public GameObject blood;
 
 	void Start(){
 		player = GameObject.FindWithTag ("Player");
@@ -12,11 +13,16 @@ public class EnemyStats : CharacterStats {
 
 
 	void OnCollisionEnter(Collision collision){
-		if (collision.gameObject.layer == 8) {
+		if (collision.gameObject.tag == "Bullet") {
 			this.TakeDamage (playerStats.damage.getValue());
+
+			//Sangrado
+			var contact = collision.contacts[0];
+			var rot = Quaternion.FromToRotation(Vector3.up, contact.normal); 
+			var newBlood = Instantiate(blood, contact.point, rot);
+
 			Destroy (collision.gameObject);
 		}
 	}
-		
     
 }
