0851620db801d0c6648a6dbee9fd0820d44cff8b
Unknown <yeraymendez07@gmail.com>
2018-05-14 00:42:21 +0200

    Zombie v1.1

---
+++ 3D/Game/Assets/Models/Scripts/IAZombie.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+[RequireComponent(typeof (Enemy1Character))]
+[RequireComponent(typeof (CharacterStats))]
+
+public class IAZombie : MonoBehaviour
+{
+	public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+	public ZombieCharacter character { get; private set; } // the character we are controlling
+	public CharacterStats stats;
+	public Transform target;                                    // target to aim for
+	public float lookRadius = 15f;
+	public float distance=20f;
+	public CharacterStats playerStats;
+	public ThirdPersonUserControl characterControl;
+	private int count = 0;
+
+	private void Start()
+	{
+		// get the components on the object we need ( should not be null due to require component so no need to check )
+		agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+		character = GetComponent<ZombieCharacter>();
+		stats = GetComponent<CharacterStats>();
+		agent.updateRotation = false;
+		agent.updatePosition = true;
+		playerStats = GameObject.FindWithTag ("Player").GetComponent<CharacterStats> ();
+	}
+	private void Update()
+	{
+
+		if (!stats.isDead ()) {
+			if (target != null)
+				distance = Vector3.Distance (target.position, transform.position);
+
+
+			if (distance <= lookRadius) {
+				agent.SetDestination (target.position);
+				character.Move (agent.desiredVelocity, false, false);
+
+				//Debug.Log("Radio inside");            
+
+				if (agent.remainingDistance < agent.stoppingDistance) {
+					characterControl.setCharacterCurrentLife (playerStats.currentHealth);
+					if (characterControl.Stop ()) {
+						//characterControl.Stop ();
+						character.Agarrar ();
+						character.DontAttack ();
+						count = 0;
+						//targetLife = playerStats.currentHealth;
+					} else {
+						if (count == 0) {
+							characterControl.setCharacterLife (playerStats.currentHealth);
+							count++;
+						}
+						character.Attack ();
+					}
+
+					// Debug.Log("Attack");
+				}
+
+			} else {
+				agent.SetDestination (agent.transform.localPosition);
+				character.Move (Vector3.zero, false, false);
+				//Debug.Log("Idle");
+			}
+		} else {
+			character.Die ();
+			agent.isStopped = true;
+		}
+	}
+
+	public void SetTarget(Transform target)
+	{
+		this.target = target;
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, lookRadius);
+	}
+
+
+}
--- 3D/Game/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ 3D/Game/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -4,72 +4,135 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
-    [RequireComponent(typeof (ThirdPersonCharacter))]
-    public class ThirdPersonUserControl : MonoBehaviour
-    {
-        private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
-        private Vector3 m_CamForward;             // The current forward direction of the camera
-        private Vector3 m_Move;
-        private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+	[RequireComponent(typeof (ThirdPersonCharacter))]
+	public class ThirdPersonUserControl : MonoBehaviour
+	{
+		private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
+		private Transform m_Cam;                  // A reference to the main camera in the scenes transform
+		private Vector3 m_CamForward;             // The current forward direction of the camera
+		private Vector3 m_Move;
+		private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+		private bool m_Block;
+		private GameObject ScapeBar;
+		private GameObject ScapeBar2;
+		private GameObject ScapeBar2Original;
+		private GameObject t1;
+		private GameObject t2;
+		private int count = 0;
+		private int currentLife = 0;
+		private int lastCurrentLife = 0;
 
-        
-        private void Start()
-        {
-            // get the transform of the main camera
-            if (Camera.main != null)
-            {
-                m_Cam = Camera.main.transform;
-            }
-            else
-            {
-                Debug.LogWarning(
-                    "Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
-                // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
-            }
+		private void Start()
+		{
+			m_Block = false;
+			ScapeBar = GameObject.Find ("Canvas/ScapeBar");
+			ScapeBar2 = GameObject.Find ("Canvas/ScapeBarRed");
+			ScapeBar2Original = ScapeBar2;
+			t1 = GameObject.Find ("Canvas/TextoScape");
+			t2 = GameObject.Find ("Canvas/TextoScape2");
+			ScapeBar.gameObject.SetActive (false);
+			ScapeBar2.gameObject.SetActive (false);
+			ScapeBar2Original.gameObject.SetActive (false);
+			t1.gameObject.SetActive (false);
+			t2.gameObject.SetActive (false);
+			// get the transform of the main camera
+			if (Camera.main != null)
+			{
+				m_Cam = Camera.main.transform;
+			}
+			else
+			{
+				Debug.LogWarning(
+					"Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+				// we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
+			}
 
-            // get the third person character ( this should never be null due to require component )
-            m_Character = GetComponent<ThirdPersonCharacter>();
-        }
+			// get the third person character ( this should never be null due to require component )
+			m_Character = GetComponent<ThirdPersonCharacter>();
+		}
 
+		public void setCharacterLife(int life){
+			lastCurrentLife = life;
+		}
 
-        private void Update()
-        {
-            if (!m_Jump)
-            {
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-            }
-        }
+		public void setCharacterCurrentLife(int life){
+			currentLife = life;
+		}
 
+		private void Update()
+		{
+			if (!m_Jump)
+			{
+				m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+			}
+		}
 
-        // Fixed update is called in sync with physics
-        private void FixedUpdate()
-        {
-            // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
-            float v = CrossPlatformInputManager.GetAxis("Vertical");
-            bool crouch = Input.GetKey(KeyCode.C);
+		public bool Stop(){
+			if ((currentLife<(lastCurrentLife-30)) && count<20)
+				m_Block = true;
+			else
+				m_Block = false; 
 
-            // calculate move direction to pass to character
-            if (m_Cam != null)
-            {
-                // calculate camera relative direction to move:
-                m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
-                m_Move = v*m_CamForward + h*m_Cam.right;
-            }
-            else
-            {
-                // we use world-relative directions in the case of no main camera
-                m_Move = v*Vector3.forward + h*Vector3.right;
-            }
-#if !MOBILE_INPUT
+			return m_Block;
+		}
+		// Fixed update is called in sync with physics
+		private void FixedUpdate()
+		{
+			Debug.Log (m_Block);
+			Debug.Log (count);
+
+			// read inputs
+			float h = CrossPlatformInputManager.GetAxis("Horizontal");
+			float v = CrossPlatformInputManager.GetAxis("Vertical");
+			bool crouch = Input.GetKey(KeyCode.C);
+
+			// calculate move direction to pass to character
+			if (m_Cam != null)
+			{
+				// calculate camera relative direction to move:
+				m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+				m_Move = v*m_CamForward + h*m_Cam.right;
+			}
+			else
+			{
+				// we use world-relative directions in the case of no main camera
+				m_Move = v*Vector3.forward + h*Vector3.right;
+			}
+			#if !MOBILE_INPUT
 			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
-#endif
+			if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+			#endif
 
-            // pass all parameters to the character control script
-            m_Character.Move(m_Move, crouch, m_Jump);
-            m_Jump = false;
-        }
-    }
+			// pass all parameters to the character control script
+			if (m_Block) {
+				if (count >= 20) {
+					m_Block = false;	
+				}
+				m_Move = new Vector3 (0, 0, 0);
+				ScapeBar.gameObject.SetActive (true);
+				t1.gameObject.SetActive (true);
+				t2.gameObject.SetActive (true);
+				if (Input.GetKey (KeyCode.Return) == true) {
+					if (count == 0) {
+						ScapeBar2Original.gameObject.SetActive (true);
+						count++;
+					} else {
+						ScapeBar2Original.transform.localScale = new Vector3 (count, 1, 1);
+						count++;
+					}
+
+				}
+			} else {
+				count = 0;
+				ScapeBar2Original = ScapeBar2;
+				ScapeBar2.gameObject.SetActive (true);
+				ScapeBar2Original.gameObject.SetActive (false);
+				ScapeBar.gameObject.SetActive (false);
+				t1.gameObject.SetActive (false);
+				t2.gameObject.SetActive (false);	
+			}
+			m_Character.Move(m_Move, crouch, m_Jump);
+			m_Jump = false;
+		}
+	}
 }
