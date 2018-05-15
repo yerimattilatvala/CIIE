fbdf830106aab9cb3f870bc40b55c3cd9f38a4bc
Unknown <yeraymendez07@gmail.com>
2018-05-16 00:22:02 +0200

    Plataformas

---
+++ 3D/Game/Assets/Models/Scripts/BridgeRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeRotation : MonoBehaviour {
+	
+	public GameObject c1,c2;
+	public GameObject b1,b2;
+	private bool update = true;
+
+	void Start(){
+		b2.SetActive (false);
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(update)
+		if (c1.activeSelf == false && c2.activeSelf == false) {
+			b2.SetActive (true);
+			b1.SetActive(false);
+		}
+	}
+}
---
+++ 3D/Game/Assets/Models/Scripts/RopesCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopesCollider : MonoBehaviour {
+
+	public GameObject rope;
+
+	void OnCollisionEnter(Collision collision){
+
+		print ("HEY");
+		if (collision.gameObject.tag == "Bullet") {
+			rope.SetActive (false);
+		}
+	}
+}
