0a5004d0105a65c4cea167ff76b47e51f399be7c
IsabelDiazDominguez <isabel.diaz.dominguez@udc.es>
2018-05-16 14:50:49 +0200

    borrado un comentario

--- 3D/Game/Assets/Models/Scripts/SceneManager/OnTriggerChangeScene.cs
+++ 3D/Game/Assets/Models/Scripts/SceneManager/OnTriggerChangeScene.cs
@@ -24,9 +24,6 @@ public class OnTriggerChangeScene : MonoBehaviour {
 				DontDestroyOnLoad (other.gameObject);
 				DontDestroyOnLoad (Camera.main.gameObject);
 				SceneManager.LoadScene(sceneIndex);
-				//Scene scene = SceneManager.GetSceneByBuildIndex(sceneIndex);
-				//SceneManager.MoveGameObjectToScene (other.gameObject, scene);
-
 				other.transform.position = new Vector3 (22.0f, -3.6f, 5.0f);
             }
             
