673d7a5de94bc42e92ffcd4382ae7a85a9a24878
Unknown <yeraymendez07@gmail.com>
2018-05-16 13:57:25 +0200

    Menu con scripts

---
+++ 3D/Game/Assets/Models/Scripts/ButtomExit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtomExit : MonoBehaviour {
+
+	public void FixedUpdate(){
+		Application.Quit ();
+	}
+}
---
+++ 3D/Game/Assets/Models/Scripts/CargarEscena1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CargarEscena1 : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	public void CargarEscena () {
+		SceneManager.LoadScene("default");
+	}
+}
